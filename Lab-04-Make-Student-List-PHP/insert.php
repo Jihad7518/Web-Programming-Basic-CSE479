713a2393df723dfa41a093a12633b796b9a5bbc8
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-08-11 21:07:25 +0600

    Update insert.php

--- Lab-04-Make-Student-List-PHP/insert.php
+++ Lab-04-Make-Student-List-PHP/insert.php
@@ -1,1 +1,19 @@
 
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $host = 'localhost';
+    $username = 'root';
+    $password = '';
+    $database = 'cse479_studentinfo';
+
+    $connection = new mysqli($host, $username, $password, $database);
+
+    if ($connection->connect_error) {
+        die('Connection failed: ' . $connection->connect_error);
+    }
+
+    // Retrieve data from the form
+    $student_name = $_POST['student_name'];
+    $student_id = $_POST['student_id'];
+    $phone_number = $_POST['phone_number'];
