b2e93e957efbdd4ee329e9e183e80c8e22cf8f6d
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-08-11 21:09:41 +0600

    Update login.php

--- Lab-04-Make-Student-List-PHP/login.php
+++ Lab-04-Make-Student-List-PHP/login.php
@@ -40,3 +40,20 @@
                 }
             }
         ?>
+                <form method="post" action="login.php" class="login-form">
+            <div class="form-group">
+                <label for="username">Username:</label>
+                <input type="text" name="username" required>
+            </div>
+
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input type="password" name="password" required>
+            </div>
+
+            <div class="form-group">
+                <input type="submit" value="Login">
+            </div>
+        </form>
+    </div>
+
