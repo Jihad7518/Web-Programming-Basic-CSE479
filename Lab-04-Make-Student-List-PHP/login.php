03a2a0f2796a1f1b20b2f46133a150c6e003e087
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-08-11 21:09:11 +0600

    Update login.php

--- Lab-04-Make-Student-List-PHP/login.php
+++ Lab-04-Make-Student-List-PHP/login.php
@@ -1,1 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
 
+    <div class="container">
+        <h1>Login</h1>
+
+        <?php
+            session_start();
+
+            // Define the valid username and password
+            $valid_username = "admin";
+            $valid_password = "admin123";
+
+            // Check if the user is already logged in
+            if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+                header("Location: index.php");
+                exit;
+            }
