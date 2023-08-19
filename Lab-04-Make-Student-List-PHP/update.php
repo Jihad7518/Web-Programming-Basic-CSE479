94648751a5ec9f64522c8be610d4eeff64ccefda
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-08-19 18:56:47 +0600

    Update update.php

--- Lab-04-Make-Student-List-PHP/update.php
+++ Lab-04-Make-Student-List-PHP/update.php
@@ -1,1 +1,23 @@
 
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Update Student</title>
+    <link rel="stylesheet" href="css/update.css">
+</head>
+<body>
+    <h1>Update Student</h1>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        $host = 'localhost';
+        $username = 'root';
+        $password = '';
+        $database = 'cse479_studentinfo';
+
+        $connection = new mysqli($host, $username, $password, $database);
+
+        if ($connection->connect_error) {
+            die('Connection failed: ' . $connection->connect_error);
+        }
