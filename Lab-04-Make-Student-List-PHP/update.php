bc576c25a33bbe232af23e17f6d74eeaab5cf84e
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-08-19 18:57:01 +0600

    Update update.php

--- Lab-04-Make-Student-List-PHP/update.php
+++ Lab-04-Make-Student-List-PHP/update.php
@@ -21,3 +21,20 @@
         if ($connection->connect_error) {
             die('Connection failed: ' . $connection->connect_error);
         }
+        // Retrieve data from the form
+        $student_id = $_POST['student_id'];
+        $student_name = $_POST['student_name'];
+        $phone_number = $_POST['phone_number'];
+
+        // Prepare and execute the SQL statement for updating the student record
+        $sql = "UPDATE student SET student_name = '$student_name', phone_number = '$phone_number' WHERE student_id = '$student_id'";
+
+        if ($connection->query($sql) === TRUE) {
+            echo "<div class='toast show-toast'>Student with ID: $student_id updated successfully.</div>";
+        } else {
+            echo "Error: " . $sql . "<br>" . $connection->error;
+        }
+
+        $connection->close();
+    }
+    ?>
