36823d0f1bb1b2b51a45683f84a9a10eaf46b555
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-08-19 18:55:41 +0600

    Update search.php

--- Lab-04-Make-Student-List-PHP/search.php
+++ Lab-04-Make-Student-List-PHP/search.php
@@ -1,1 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Search Student</title>
+    <link rel="stylesheet" href="css/search.css">
+</head>
+<body>
+<!-- Rest of the search.php code here -->
+<h1>Search Student</h1>
 
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Database connection and search code here
+    $host = 'localhost'; // Your MySQL server hostname
+    $username = 'root'; // Your MySQL username
+    $password = ''; // Your MySQL password
+    $database = 'cse479_studentinfo'; // The name of the database you created
+
+    $connection = new mysqli($host, $username, $password, $database);
+
+    if ($connection->connect_error) {
+        die('Connection failed: ' . $connection->connect_error);
+    }
