680f18adbbaa2e2b9305b39a663ec474ae54c0b4
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-08-29 00:52:11 +0600

    Add files via upload

---
+++ Java Script Problem for Final/sortIntegerDescending_7.js
@@ -0,0 +1,8 @@
+const num1 = 4;
+const num2 = 0;
+const num3 = -1;
+
+const sortedArray = [num1, num2, num3].sort((a, b) => b - a); // Change a - b to b - a
+const result = sortedArray.join(', ');
+
+alert(`Sorted result: ${result}`);
